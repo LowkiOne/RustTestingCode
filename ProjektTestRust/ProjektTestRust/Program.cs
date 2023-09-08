963a129f6943060cc27de6aa15de91db8c7d7466
LowkiOne <101903927+LowkiOne@users.noreply.github.com>
2023-09-08 06:52:08 +0200

    Add files via upload

---
+++ ProjektTestRust/ProjektTestRust/Program.cs
@@ -0,0 +1,34 @@
+﻿public class FarmingCompAndNodes
+{
+    public string WeaponCompsNames { get; set; }
+    public string ClothingCompNames { get; set; }
+    public string ItemCraftCompNames { get; set; }
+    public string OtherCompsNames { get; set; }
+    public string NodesFarmingNames { get; set; }
+    public string AnimalFarmingNames { get; set; }
+
+}
+
+public class Crafting
+{
+    public string PrimTierWeaponNames { get; set; }
+    public string MidTireWeaponsNames { get; set; }
+    public string HighTierWeaponsNames { get; set; }
+
+    public string ClothingPrimTierNames { get; set; }
+    public string LeatherPrimTierNames { get; set; }
+    public string RoadSignMidTierNames { get; set; }
+    public string HighQualityHighTierNames { get; set; }
+
+    public string DefencePlaceingItemsNames { get; set; }
+    public string ElectonicItemsNames { get; set; }
+
+}
+
+public class AmmoAndRaid
+{
+    public string PrimAmmoNames { get; set; }
+    public string HighAmmoNames { get; set; }
+    public string AllRaidExplosivesNames { get; set; }
+
+}

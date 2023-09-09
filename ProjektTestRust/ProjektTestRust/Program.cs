f42106977a35281d8c3c9eea155ca90703307cd3
LowkiOne <101903927+LowkiOne@users.noreply.github.com>
2023-09-09 20:46:44 +0200

    Add files via upload

---
+++ ProjektTestRust/ProjektTestRust/Program.cs
@@ -0,0 +1,13 @@
+﻿public class AllItemsInRust
+{
+    public string WeaponsNames { get; set; }
+    public string AmmoNames { get; set; }
+    public string ArmorNames { get; set; }
+    public string FarmingResorcesNames { get; set;}
+    public string CraftingResorcesNames { get; set;}
+    public string ComponentNames { get; set; }
+    public string ItemsCraftedNames { get; set;}
+    public string ElectronicItemsNames { get; set;}
+    public string BuildingNames { get; set;}
+
+}
